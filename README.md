46c0d258735500e1d83aeec5b00ed417a8ce6148
Michael Monsour <michael@lethek.net>
2022-11-24 13:56:29 +1000

    Added a very basic example to documentation

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # SideData
 Attach data to any object, for the lifetime of that object.
+
+## Example Basic Usage
+
+```csharp
+using SideData;
+
+var obj = new { Name = "Some object" };
+obj.SideData().Shape = "Rectangle";
+obj.SideData().NumberOfEdges = 4;
+obj.SideData().EdgeLengths = new int[] { 6, 4, 6, 4 };
+
+Console.WriteLine($"A {obj.SideData().Shape} has {obj.SideData().NumberOfEdges} sides");
+```
+
+See the unit tests for a few more examples
--- tests/SideData.Tests/SideDataTests.cs
+++ tests/SideData.Tests/SideDataTests.cs
@@ -9,7 +9,25 @@ public class SideDataTests : IClassFixture<TestDataFixture>
 
 
     [Fact]
-    public void DynamicData_UsesSideData()
+    public void SideData_ForEachHostObject_IsUniquePerHostObject()
+    {
+        var user1 = Fixture.UserFaker.Generate();
+        var user1FirstSideData = user1.SideData();
+        var user1SecondSideData = user1.SideData();
+        Assert.Same(user1FirstSideData, user1SecondSideData);
+
+        var user2 = Fixture.UserFaker.Generate();
+        var user2FirstSideData = user2.SideData();
+        var user2SecondSideData = user2.SideData();
+        Assert.Same(user2FirstSideData, user2SecondSideData);
+
+        //SideData for user1 should be different to the SideData for user2
+        Assert.NotSame(user1FirstSideData, user2FirstSideData);
+    }
+
+
+    [Fact]
+    public void SetAndGet_MultipleSideData_ReturnsSameValues()
     {
         var user = Fixture.UserFaker.Generate();
         var primaryContact = Fixture.ContactDataFaker.Generate();
